8923a5ab1ef09a0084bc37cd273b513fd7c9655c
Alex Razoumov <alex.razoumov@westgrid.ca>
2018-12-14 19:40:09 -0800

    added 2018-12-18-competition-results

---
+++ _posts/2018-12-18-competition-results.md
@@ -0,0 +1,66 @@
+---
+layout: post
+title: "Announcing winners"
+author: "WestGrid"
+categories: journal
+tags: [documentation,sample]
+image: topTwo.png
+---
+
+2018 Visualize This! Challenge was hosted by WestGrid, a regional partner of Compute Canada. 31 people
+and groups expressed interest in participating in the competition this year, and we had some interesting
+submissions. After thoughtful deliberation the jury has picked three winners.
+
+### First place
+
+The first place was taken by Philippe Nazair, a Data Visualization Developer at Université du Québec à
+Rimouski. Philippe's visualization is implemented as a static website with two layers: a graph viewer
+using the [3d-force-graph web component](https://github.com/vasturiano/3d-force-graph), and a map viewer
+implemented with [Leaflet](https://leafletjs.com) to display markers on the map and written biographies
+from [Cambridge's Orlando](http://orlando.cambridge.org) author's page found in the dataset. Below you
+can see a screen-capture video showing different interactions on this site. All JSON files for the
+visualization were created with a Python backend. We especially appreciated sample graphs for radical and
+liberal ideologies and other groups, as well as the ability to build more complex graphs by selecting a
+predicate and setting it to a certain value, e.g. `hasActiveInvolvementIn = socialism`, or multiple
+values; each graph is limited to one predicate. Individual graph nodes can be displayed as either text or
+draggable spheres
+
+<div class="flex-video">
+	<iframe width="640" height="350" src="https://player.vimeo.com/video/306261749" frameborder="0"
+	allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+	allowFullScreen mozallowfullscreen webkitAllowFullScreen></iframe>
+</div>
+
+Or click <a href="https://vimeo.com/306261749">here</a> to watch this video directly on Vimeo.
+
+### Second place
+
+The second prize went to Usman Alim and Roberta Cabral Ramos Mota from the Department of Computer Science
+at the University of Calgary. Below you can watch one of their visualizations (rendered with OSPRay in
+ParaView) showing the protein cavity and two "highly-interacting" PO4 beads with the colour representing
+time step. This work was selected for their use of Python scripting for the bulk of the analysis: using
+[MDAnalysis](https://www.mdanalysis.org) library for identifying closely interacting membrane beads, and
+writing bead positions in VTK to be rendered in ParaView. You can download an edited version of their
+Python analysis script [here]( {{ site.baseurl }}/analysis.py).
+
+<div class="flex-video">
+	<iframe width="640" height="350" src="https://player.vimeo.com/video/306277152" frameborder="0"
+	allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+	allowFullScreen mozallowfullscreen webkitAllowFullScreen></iframe>
+</div>
+
+Or click <a href="https://vimeo.com/306277152">here</a> to watch this video directly on Vimeo.
+
+### Third place
+
+The third place was taken by Catherine Winters from the Digital Humanities Innovation Lab at Simon Fraser
+University. This visualization is powered by [D3.js](https://d3js.org) and
+[Three.js](https://threejs.org), with all JSON data files produced with Python scripting. This
+visualization is still in development, but has the promise to show clustering based on ideology,
+religion, or geographic proximity.
+
+### Acknowledgments
+
+The top prize for this competition -- a 43" 4K Multi-Client Monitor -- was generously donated by Dell
+EMC. We would like to acknowledge Dell EMC for their continued support of Advanced Research Computing in
+Canada!
---
+++ analysis.py
@@ -0,0 +1,99 @@
+# Code fragments from WestGrid's 2018 Visualize This! submission by Usman Alim and Roberta Cabral Ramos
+# Mota (Dept. of Computer Science, University of Calgary).
+
+import MDAnalysis as mda
+import numpy as np
+from MDAnalysis.analysis.density import density_from_Universe
+from MDAnalysis.analysis.distances import distance_array
+import tvtk.api as tvtk
+
+base = '/Users/razoumov/Documents/visualizeThis/2018/data/scientific/'
+u = mda.Universe(base+'sim.part0001_protein_lipid_popc.gro', [base+'5us_traj_popc_protein_lipid_popc.xtc',base+'15us_traj_protein_lipid_popc.xtc'])
+
+# define the contact condition in ångströms
+
+contact_dist = 5
+nearby = "name PO4 and (around " + str(contact_dist) + " name SC1 or around " + str(contact_dist) + " name SC2 or around " + str(contact_dist) + " name SC3 or around " + str(contact_dist) + " name SC4)"
+
+# write out 3D density of PO4 everywhere and near the protein in VTK Image Data
+
+Dall = density_from_Universe(u, delta=0.5, atomselection="name PO4", update_selection=True)
+Dnear = density_from_Universe(u, delta=1, atomselection=nearby, update_selection=True)
+
+print('near:', Dnear.grid.shape)   # should be (60, 57, 51)
+grid = tvtk.tvtk.ImageData(spacing=Dnear.delta, origin=Dnear.origin, dimensions=Dnear.grid.shape)
+grid.point_data.scalars = Dnear.grid.ravel(order='F')
+grid.point_data.scalars.name = 'POPC near SC1_4'
+tvtk.write_data(grid, 'Dnear.vtk')   # omit `vtk` extension to write newer XML VTK, otherwise legacy VTK
+
+print('all:', Dall.grid.shape)   # should be (302, 298, 124)
+grid = tvtk.tvtk.ImageData(spacing=Dall.delta, origin=Dall.origin, dimensions=Dall.grid.shape)
+grid.point_data.scalars = Dall.grid.ravel(order='F')
+grid.point_data.scalars.name = 'POPC all'
+tvtk.write_data(grid, 'Dall.vtk')   # omit `vtk` extension to write newer XML VTK, otherwise legacy VTK
+
+### identify membrane's PO4 beads that interact with the protein, and classify these into high-, medium-,
+### and low-interacting
+
+selection = u.select_atoms(nearby, updating=True)
+clist = []   # will contain a list of 1102 frames, each frame listing PO4 beads (IDs) close to protein's SC{1..4}
+for timestep in u.trajectory:
+    clist.append(set(selection.ids))
+
+itrajs = dict();   # a dictionary of PO4 beads (IDs) that stay close to protein's SC{1..4} for 5, 6, ... frames
+for wsize in range(5,25):
+    bindList = []
+    for i in range(0,len(clist) - wsize):
+        bindList.append(set.intersection(*clist[i:i+wsize]))
+    itrajs[wsize] = set.union(*bindList)
+
+# partition the trajectories into groups
+A = set.intersection(itrajs[5],  itrajs[6],  itrajs[7])
+B = set.intersection(itrajs[8],  itrajs[9],  itrajs[10])
+C = set.intersection(itrajs[11], itrajs[12], itrajs[13])
+
+LOW = A - B - C
+MID = B - C
+HIGH = C
+
+print("Number of HIGH PO4 beads: " + str(len(HIGH)))
+print("Number of MEDIUM PO4 beads: " + str(len(MID)))
+print("Number of LOW PO4 beads: " + str(len(LOW)))
+
+# for each MEDIUM PO4 bead, write a VTK file with all its positions and the shortest distance to a
+# protein's SC{1..4} bead
+
+prefix = "mid"           # change to 'high' for HIGH beads
+for id in sorted(MID):   # change to HIGH for HIGH beads
+    atom_selection = u.select_atoms("bynum " + str(id), updating=True)
+    protein_selection = u.select_atoms("name SC1 or name SC2 or name SC3 or name SC4", updating=True)
+    points = tvtk.tvtk.Points()
+    distances = tvtk.tvtk.FloatArray()
+    distances.name = 'distances'
+    times = tvtk.tvtk.FloatArray()
+    times.name = 'TIME'
+    for i, timestep in enumerate(u.trajectory):
+        d = distance_array(atom_selection.positions, protein_selection.positions)
+        if d.min() <= contact_dist:
+            points.insert_next_point(tuple(timestep.positions[id]))
+            distances.insert_next_value( d.min() )
+            times.insert_next_value( i )
+
+    polyline = tvtk.tvtk.PolyData()
+    polyline.trait_set(points=points)
+    polyline.point_data.scalars = distances
+    polyline.point_data.add_array(times)
+    tvtk.write_data(polyline, prefix + str(id) + ".vtk")
+
+# for each of 1102 frames, write a VTK file `high/HIGH?????.vtk` that contains the positions of all HIGH
+# PO4 beads at that timestep
+
+for i, timestep in enumerate(u.trajectory):
+    points = tvtk.Points()
+    polyData = tvtk.PolyData()
+    for id in HIGH:
+        atom_selection = u.select_atoms("bynum " + str(id), updating=True)
+        points.insert_next_point(tuple(timestep.positions[id]))
+        
+    polyData.trait_set(points=points)
+    write_data(polyData, "high/HIGH" + "{:0>5d}".format(i) + ".vtk")
